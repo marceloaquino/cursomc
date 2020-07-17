79680dc369ccb81c27821e00e903c877bd8e792b
Marcelo Aquino <macloureiro@outlook.com>
2020-07-17 17:44:42 -0300

    Cliente, TipoCliente, telefones e enderecos com Endpoint /clientes/{id} disponivel

--- src/main/java/com/marceloaquino/cursomc/CursomcApplication.java
+++ src/main/java/com/marceloaquino/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marceloaquino.cursomc.domain.Categoria;
 import com.marceloaquino.cursomc.domain.Cidade;
+import com.marceloaquino.cursomc.domain.Cliente;
+import com.marceloaquino.cursomc.domain.Endereco;
 import com.marceloaquino.cursomc.domain.Estado;
 import com.marceloaquino.cursomc.domain.Produto;
+import com.marceloaquino.cursomc.domain.enums.TipoCliente;
 import com.marceloaquino.cursomc.repositories.CategoriaRepository;
 import com.marceloaquino.cursomc.repositories.CidadeRepository;
+import com.marceloaquino.cursomc.repositories.ClienteRepository;
+import com.marceloaquino.cursomc.repositories.EnderecoRepository;
 import com.marceloaquino.cursomc.repositories.EstadoRepository;
 import com.marceloaquino.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -66,8 +75,17 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("34682695","988785733"));
 		
-	
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 	}
 
 }
--- src/main/java/com/marceloaquino/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/marceloaquino/cursomc/resources/exception/StandardError.java
@@ -39,7 +39,5 @@ public class StandardError implements Serializable {
 	public void setTimeStamp(Long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-	
-	
 
 }
